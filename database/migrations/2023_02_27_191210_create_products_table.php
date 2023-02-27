bd71436862729b88c8bee39459ae216aa8846e20
unknown <fixik2019@gmail.com>
2023-02-27 23:43:00 +0300

    adding product migration

---
+++ database/migrations/2023_02_27_191210_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->decimal('main_price');
+            $table->integer('sale')->nullable();
+            $table->boolean('is_stock');
+            $table->text('description');
+
+            $table->unsignedBigInteger('category_id');
+            $table->index('category_id', 'product_category_idx');
+            $table->foreign('category_id', 'product_category_fk')->references('id')->on('categories');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
